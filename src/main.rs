436e672bc3e1d7ea8824e9ebacb0d1b2269cfb95
vicsnet <adesanmiadebisi@gmail.com>
2024-02-12 19:10:48 +0100

    looping through collection with for

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ fn main() {
 
     let number = 7;
 
-    if number <5{
+    if number < 5 {
         println!("condition was true");
-
-    }else{
+    } else {
         println!("Condition ws false");
     }
 
@@ -19,55 +18,55 @@ fn main() {
     multiple_loop();
 
     conditional_loop_with_while();
+    looping_with_for();
 }
-fn multiple_condition(){
+fn multiple_condition() {
     let number = 6;
 
-    if number % 4 == 0{
+    if number % 4 == 0 {
         println!("number is divisible by 4");
-
-    }else if number % 3 == 0{
+    } else if number % 3 == 0 {
         println!("number is divisible by 3");
-    }else if number % 2 == 0{
+    } else if number % 2 == 0 {
         println!("number is divisible by 2");
-    }else{
+    } else {
         println!("number is not divisible by 4, 3, or 2");
     }
 }
 
-fn if_in_let_statement(){
+fn if_in_let_statement() {
     let condition = true;
-    let number = if condition {5} else {6};
+    let number = if condition { 5 } else { 6 };
 
     println!("The value of number is : {number}");
 }
 
-fn repetition_in_loop(){
+fn repetition_in_loop() {
     let mut counter = 0;
-let result  = loop {
-    counter +=1;
-    println!("{counter}");
-    if counter == 10{
-        break counter * 2;
-    }
-};
+    let result = loop {
+        counter += 1;
+        println!("{counter}");
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
 
-println!("the result is {result}");
+    println!("the result is {result}");
 }
 
-fn multiple_loop(){
+fn multiple_loop() {
     let mut count = 0;
 
-    'counting_up: loop{
+    'counting_up: loop {
         println!("count = {count}");
         let mut remaining = 10;
 
-        loop{
+        loop {
             println!("remianing = {remaining}");
             if remaining == 9 {
                 break;
             }
-            if count == 2{
+            if count == 2 {
                 break 'counting_up;
             }
             remaining -= 1;
@@ -76,12 +75,33 @@ fn multiple_loop(){
     }
     println!("End count = {count}");
 }
-fn conditional_loop_with_while(){
+fn conditional_loop_with_while() {
     let mut number = 3;
-    while number != 0{
+    while number != 0 {
         println!("{number}!");
         number -= 1;
     }
 
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40];
+
+    let mut index = 0;
+
+    while index < 4 {
+        // this approach is error prone
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn looping_with_for() {
+    // looping through a collection with for
+
+    let b = [10, 20, 30, 40];
+
+    for element in b {
+        println!("the value in element is: {element}");
+    }
 }
